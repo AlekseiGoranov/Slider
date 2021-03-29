37ae55f08ea1eada0ee9e3f5ea8c06b4678b1d98
Aleksei <goranov93@mail.ru>
2021-03-29 10:41:28 +0300

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Aleksei Goranov on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
